9c8c8a4b68b5184b0a925651be00f2c27fcd97f4
OMGFriendly <kevin22328@gmail.com>
2022-11-30 00:33:17 +0100

    .

--- config.lua
+++ config.lua
@@ -1,11 +1,11 @@
 Config = {}
 Config.DisableAmbience = false -- Disabled distance sirens, distance car alarms, etc
 
-Config.ParkedVehicleMultiplier = 0.6
-Config.PedDensityMulitplier = 0.1
-Config.RandomVehicleMultiplier = 0.2
-Config.VehicleMultiplier = 0.2
-Config.ScenarioPedMultiplier = 0.2
+Config.ParkedVehicleMultiplier = 0.0
+Config.PedDensityMulitplier = 0.0
+Config.RandomVehicleMultiplier = 0.0
+Config.VehicleMultiplier = 0.0
+Config.ScenarioPedMultiplier = 0.0
 
 Config.BlacklistedScenarios = {
     ['TYPES'] = {
@@ -25,49 +25,5 @@ Config.BlacklistedScenarios = {
 }
 
 Config.BlacklistedVehs = {
-    [`SHAMAL`] = true,
-    [`LUXOR`] = true,
-    [`LUXOR2`] = true,
-    [`JET`] = true,
-    [`LAZER`] = true,
-    [`BUZZARD`] = true,
-    [`BUZZARD2`] = true,
-    [`ANNIHILATOR`] = true,
-    [`SAVAGE`] = true,
-    [`TITAN`] = true,
-    [`RHINO`] = true,
-    [`FIRETRUK`] = true,
-    [`MULE`] = true,
-    [`MAVERICK`] = true,
-    [`BLIMP`] = true,
-    [`AIRTUG`] = true,
-    [`CAMPER`] = true,
-    [`HYDRA`] = true,
-    [`OPPRESSOR`] = true,
-    [`technical3`] = true,
-    [`insurgent3`] = true,
-    [`apc`] = true,
-    [`tampa3`] = true,
-    [`trailersmall2`] = true,
-    [`halftrack`] = true,
-    [`hunter`] = true,
-    [`vigilante`] = true,
-    [`akula`] = true,
-    [`barrage`] = true,
-    [`khanjali`] = true,
-    [`caracara`] = true,
-    [`blimp3`] = true,
-    [`menacer`] = true,
-    [`oppressor2`] = true,
-    [`scramjet`] = true,
-    [`strikeforce`] = true,
-    [`cerberus`] = true,
-    [`cerberus2`] = true,
-    [`cerberus3`] = true,
-    [`scarab`] = true,
-    [`scarab2`] = true,
-    [`scarab3`] = true,
-    [`rrocket`] = true,
-    [`ruiner2`] = true,
-    [`deluxo`] = true,
+    
 }
